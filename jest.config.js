55a41265dfe75e45d5de1ab748fb87c3f2ba4c19
momsse <momssesystem@gmail.com>
2019-08-11 13:16:55 +0200

    :sparkles: upgrade with jest and circle ci

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testPathIgnorePatterns: ['dist'],
+};
